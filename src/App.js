6c6236c279f6ff27fe41e0322ad0eb1458ed47ce
kimanicharles911 <kimanicharles911@gmail.com>
2021-04-21 10:05:07 +0300

    render first products with details

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import ProductComponent from "./components/ProductComponent.jsx";
 
 function App() {
 
-  const [productData, setProductData] = [
-
-  ];
+  const [productsData, setProductsData] = useState([
+    {name: "Oppo", cost: 13, quantity: 4},
+    {name: "Redmi", cost: 15, quantity: 6},
+    {name: "Huawei", cost: 17, quantity: 8}
+  ]);
 
   return (
     <div className="body-section">
-      <ProductComponent />
+      {productsData.map(productData => <ProductComponent name={productData.name} cost={productData.cost} quantity={productData.quantity}/>)}
     </div>
   );
 }

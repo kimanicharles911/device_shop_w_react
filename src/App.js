944214e80cf2480e42023c1efbbfea91ea5b078f
kimanicharles911 <kimanicharles911@gmail.com>
2021-04-26 12:20:07 +0300

    add arr to name of num cart state & associated arrays

--- src/App.js
+++ src/App.js
@@ -18,17 +18,15 @@ function App() {
     {name: "Oppo", cost: 13, photo: <img src={OppoPhoto} width="100px" height="100px" alt="Oppo_Photo" />,  quantity: 0 },
     {name: "Redmi", cost: 15, photo: <img src={RedmiPhoto} width="100px" height="100px" alt="RedMi_Photo" />, quantity: 0 },
     {name: "Huawei", cost: 17, photo: <img src={HuaweiPhoto} width="100px" height="100px" alt="Huawei_Photo" />,  quantity: 0 },
-    {name: "IPhone", cost: 23 , photo: <img src={IPhonePhoto} width="100px" height="100px" alt="IPhone Photo" />,  quantity: 0 },
-    {name: "Xiaomi", cost: 17 , photo: <img src={XiaomiPhoto} width="100px" height="100px" alt="Xiaomi Photo" />,  quantity: 0 },
-    {name: "Samsung S7", cost: 21 , photo: <img src={SamsungS7Photo} width="100px" height="100px" alt="Samsung S7 Photo" />,  quantity: 0 },
-    {name: "HP Spectre", cost: 200 , photo: <img src={HPSpectrePhoto} width="100px" height="100px" alt="HP Spectre Photo" />,  quantity: 0 },
-    {name: "Dell", cost: 175 , photo: <img src={DellPhoto} width="100px" height="100px" alt="Dell Photo" />,  quantity: 0 },
-    {name: "Acer Swift", cost: 150 , photo: <img src={AcerSwiftPhoto} width="100px" height="100px" alt="Acer Swift Photo" />,  quantity: 0 },
-    {name: "Asus", cost: 135 , photo: <img src={AsusPhoto} width="100px" height="100px" alt="Asus Photo" />,  quantity: 0 }
+    {name: "IPhone", cost: 23 , photo: <img src={IPhonePhoto} width="100px" height="100px" alt="IPhone_Photo" />,  quantity: 0 },
+    {name: "Xiaomi", cost: 17 , photo: <img src={XiaomiPhoto} width="100px" height="100px" alt="Xiaomi_Photo" />,  quantity: 0 },
+    {name: "Samsung S7", cost: 21 , photo: <img src={SamsungS7Photo} width="100px" height="100px" alt="Samsung S7_Photo" />,  quantity: 0 },
+    {name: "HP Spectre", cost: 200 , photo: <img src={HPSpectrePhoto} width="100px" height="100px" alt="HP Spectre_Photo" />,  quantity: 0 },
+    {name: "Dell", cost: 175 , photo: <img src={DellPhoto} width="100px" height="100px" alt="Dell_Photo" />,  quantity: 0 },
+    {name: "Acer Swift", cost: 150 , photo: <img src={AcerSwiftPhoto} width="100px" height="100px" alt="Acer Swift_Photo" />,  quantity: 0 },
+    {name: "Asus", cost: 135 , photo: <img src={AsusPhoto} width="100px" height="100px" alt="Asus_Photo" />,  quantity: 0 }
   ]); 
-  const [numCart, setNumCart] = useState([]);
-  const [numBill, setNumBill] = useState([]);
-  const [checkoutAmount, setCheckoutAmount] = useState();
+  const [arrNumCart, arrSetNumCart] = useState([]);
   const [arrCheckoutAmount, setArrCheckoutAmount] = useState([]);
 
   const handleProductQuantityChange = ({ name, quantity}) => {
@@ -38,11 +36,11 @@ function App() {
     setProductsData(newProductList);
   };
 
-  const handleAddToCart = ( theQuantity, name, cost ) => {
-    const newNumCart = [...numCart];
+  const handleAddToCart = ( theQuantity, name ) => {
+    const newArrNumCart = [...arrNumCart];
     const newSum = theQuantity;
-    newNumCart.push(newSum);
-    setNumCart(newNumCart);
+    newArrNumCart.push(newSum);
+    arrSetNumCart(newArrNumCart);
     
     console.log("Kaka" + name);
     const xProdIndex = productsData.findIndex(x => x.name === name);
@@ -54,16 +52,8 @@ function App() {
 
   };
 
-  const quantitySum = numCart.reduce((x, y ) => {
+  const quantitySum = arrNumCart.reduce((x, y ) => {
     return x + y;
-  }, 0);
-
-  const bill = productsData.reduce((result, {cost, quantity = 0}) => {
-    return result += quantity * cost;
-  }, 0);
-
-  const billTwo = productsData.reduce((result, {quantity = 0}) => {
-    return result += quantity;
   }, 0);
   
   let pushProductCost = [];
@@ -74,17 +64,13 @@ function App() {
     return x +y;
   }, 0);
 
-  const checkout = () => {
-    setCheckoutAmount(bill)
-  };
-
   const numCheckoutAmount = arrCheckoutAmount.reduce((x, y ) => {
     return x + y;
   }, 0);
 
   return (
     <div className="body-section">
-    <div> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 🛒 {quantitySum} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Total Bill 💲{numCheckoutAmount} &nbsp; &nbsp; <button onClick={checkout}>Proceed to Checkout</button> {/* &nbsp; &nbsp; 💲 ignore: {bill} of {billTwo}&nbsp; &nbsp; 💲 ignore: {sumProductCost} */} </div>     
+      <div> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 🛒 {quantitySum} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Total Bill 💲{numCheckoutAmount} &nbsp; &nbsp; <button>Proceed to Checkout</button> {/* &nbsp; &nbsp; 💲 ignore: {bill} of {billTwo}&nbsp; &nbsp; 💲 ignore: {sumProductCost} */} </div>     
       {productsData.map((productData, i) => <ProductComponent key={i} name={productData.name} cost={productData.cost} photo={productData.photo} onQuantityChange={handleProductQuantityChange} onClickAddToCart={handleAddToCart}/>)}
     </div>
   );
